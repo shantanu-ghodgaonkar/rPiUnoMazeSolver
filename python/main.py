0ddfefb85830b7e72a8d810e98f93e2aa5e41f9d
shantanu-ghodgaonkar <shantanu.ghodgaonkar@gmail.com>
2024-04-27 10:45:40 -0400

    Restructuring main.py

--- python/main.py
+++ python/main.py
@@ -1,8 +1,11 @@
 import serial
 arduino = '/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_24238313635351F0A162-if00'
 baudrate = 115200
-ser = serial.Serial(arduino, baudrate)
-while 1:
-    #print(ser.readline())
-    count = input("How many times should the LED blink? ")
-    ser.write(count.encode('ascii'))
+
+
+if __name__ == "__main__":
+    ser = serial.Serial(arduino, baudrate)
+    while 1:
+        # print(ser.readline())
+        count = input("How many times should the LED blink? ")
+        ser.write(count.encode('ascii'))
