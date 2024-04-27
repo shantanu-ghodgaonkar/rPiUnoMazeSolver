27740cdc6bac19930b3a0083d06a08342db4781d
shantanu-ghodgaonkar <shantanu.ghodgaonkar@gmail.com>
2024-04-27 11:30:15 -0400

    Added logic for camera. Testing remains

--- python/main.py
+++ python/main.py
@@ -1,11 +1,23 @@
 import serial
+from time import sleep
+from pathlib import Path
+from picamera import PiCamera
+from io import BytesIO
+
 arduino = '/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_24238313635351F0A162-if00'
 baudrate = 115200
-
+camera = PiCamera()
 
 if __name__ == "__main__":
     ser = serial.Serial(arduino, baudrate)
+    camera.resolution = (1280, 720)
+    camera.exposure_compensation = 2
+    camera.exposure_mode = 'auto'
+    camera.meter_mode = 'average'
+    camera.image_effect = 'none'
     while 1:
+        camera.start_preview()
+        camera.capture(Path.joinpath(Path(__file__).parent.resolve(), 'img', 'capture.jpg'))
         # print(ser.readline())
-        count = input("How many times should the LED blink? ")
+        count = input("How to turn the servo? ")
         ser.write(count.encode('ascii'))
