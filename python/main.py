b309041b7e55597611f7508093cad293e1680e9c
shantanu-ghodgaonkar <shantanu.ghodgaonkar@gmail.com>
2024-04-27 10:23:57 -0400

    Basic structure formation

--- python/main.py
+++ python/main.py
@@ -1,7 +1,7 @@
 import serial
 arduino = '/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_24238313635351F0A162-if00'
 baudrate = 115200
-ser = serial.Serial(arduino, baudrate);
+ser = serial.Serial(arduino, baudrate)
 while 1:
     #print(ser.readline())
     count = input("How many times should the LED blink? ")
