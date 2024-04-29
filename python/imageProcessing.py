16369a40aac2bf621d7d7969025acf530c9306da
shantanu-ghodgaonkar <shantanu.ghodgaonkar@gmail.com>
2024-04-29 16:10:57 -0400

    image Processing code in progress

---
+++ python/imageProcessing.py
@@ -0,0 +1,32 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+class imageProcessing:
+
+    def __init__(self, imgPath: str):
+        img = cv.imread(imgPath,cv.IMREAD_COLOR) #Reading Image as BGR
+
+    def process_image
+b,g,r=cv.split(img) #Saved image is in RBG decomposing RGB
+img=cv.merge((r,g*0,b*0)) #Merging RGB as BGR and eliminating G and B components
+img=cv.addWeighted(img,2,img,0,0) #Increasing contrast
+img=cv.cvtColor(img,cv.COLOR_BGR2GRAY) #Converting to grayscale
+mask = cv.inRange(img, 100, 255);
+_,img = cv.threshold(img,20,255,cv.THRESH_BINARY) #Thresholding image to convert to BW
+
+
+kernel = np.ones((10,10),np.uint8) #Setting kernel for morphing
+img = cv.morphologyEx(img, cv.MORPH_CLOSE, kernel) #Close morphing (Dilation followed by erosion eliminates black dots)
+
+kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE,(15,15)) #Setting kernel for morphing
+#kernel = np.ones((10,10),np.uint8) #Setting kernel for morphing
+img = cv.morphologyEx(img, cv.MORPH_OPEN, kernel)  #Open morphing (Erosion followed by Dilation eliminates white dots)
+
+img = cv.Canny(img,0,255) #Canny edge detection
+
+
+plt.imshow(img,'gray')
+plt.show()
+
--- python/main.py
+++ python/main.py
@@ -1,34 +1,32 @@
 import serial
 from time import sleep
 from pathlib import Path
-from picamera import PiCamera
+from picamera2 import Picamera2, Preview
 
 arduino = '/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_24238313635351F0A162-if00'
 baudrate = 115200
-camera = PiCamera()
+camera = Picamera2()
 
 if __name__ == "__main__":
 #    ser = serial.Serial(arduino, baudrate)
-    camera.resolution = (1280, 720)
-    camera.exposure_compensation = 2
-    camera.exposure_mode = 'auto'
-    camera.meter_mode = 'average'
-    camera.image_effect = 'none'
     imagePath = Path.joinpath(Path(__file__).parent.resolve(), 'img', 'capture.jpg')
-    camera.start_preview()
+    
+    camera.configure(camera.create_preview_configuration()) # type: ignore
+    camera.start_preview(Preview.QTGL) # type: ignore
+    camera.start()
     while 1: # Change condition to maze solved or not
-        camera.capture(imagePath)
-        # print(ser.readline())
-        count = input("How to turn the servo? ")
-        ser.write(count.encode('ascii'))
+        camera.capture_file(imagePath)
+    #     # print(ser.readline())
+    #     count = input("How to turn the servo? ")
+    #     ser.write(count.encode('ascii'))
 
-        # Use imageProcessing class to process captured image into greyscale
-        # Use greyscale image in mazeDetector and ballDetector
-        # Output of mazeDetector is the maze and the end point of the maze
-        # Output of the ballDetector is the start point of the maze
-        # Outputs of mazeDetector and ballDetector are fed to mazeSolver
-        # mazeSolver returns the path which is going to be followed by the ball in the main
-    camera.annotate_text_size = 100
-    camera.annotate_text = "MAZE SOLVED!"
-    camera.capture(imagePath)
+    #     # Use imageProcessing class to process captured image into greyscale
+    #     # Use greyscale image in mazeDetector and ballDetector
+    #     # Output of mazeDetector is the maze and the end point of the maze
+    #     # Output of the ballDetector is the start point of the maze
+    #     # Outputs of mazeDetector and ballDetector are fed to mazeSolver
+    #     # mazeSolver returns the path which is going to be followed by the ball in the main
+    # camera.annotate_text_size = 100
+    # camera.annotate_text = "MAZE SOLVED!"
+    # camera.capture(imagePath)
 
